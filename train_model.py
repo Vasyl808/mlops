0e2e468cd266ee008f245ba4cee6691fc15be3f7
Vasyl Hunia <113974753+Vasyl808@users.noreply.github.com>
2024-05-20 15:59:05 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -103,9 +103,9 @@ def automated_model_retrain_validation_selection(data, categorys, numeric_column
 
     input_shape = (X_train.shape[1],)
     model = create_model(input_shape)
-
+    mlflow.set_tracking_uri('sqlite:////tmp/mlruns.db')
+    
     with mlflow.start_run() as run:
-        mlflow.set_tracking_uri('sqlite:////tmp/mlruns.db')
         mlflow.tensorflow.autolog()
 
         dataset_train = mlflow.data.from_pandas(
